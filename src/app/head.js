b105a79a45f6d3f8a1c0284017321a702e6bf081
mycomputer <muhammadarslanjabbar@gmail.com>
2025-06-02 22:37:22 +0500

    meta tag

---
+++ src/app/head.js
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="linkweave-verification" content="d5ce81c532a6b3e2956a7362cc078995d8923dd917962eb55ee00a579d4035b5" />
+    </>
+  );
+}
