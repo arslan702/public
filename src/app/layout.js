ae124fcfeae09114dbd973ca2cf02d64ac8004aa
mycomputer <muhammadarslanjabbar@gmail.com>
2025-07-03 17:38:51 +0500

    tag-update

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
   title: "Where committed investors invest",
   description: "Built for investors who mean business",
   other: {
-    "backlinkexchange-verification": "d2076d63-4dd8-493a-a398-c985ba964399"
+    "backlinkexchange-verification": "cd2a3af3-d55a-443b-b5d6-d3953aab5fbd"
   }
 };
 
