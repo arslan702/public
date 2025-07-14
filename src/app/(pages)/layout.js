23d6f7eea2b04f119097c352ede13b358dcd091c
mycomputer <muhammadarslanjabbar@gmail.com>
2025-07-14 16:18:47 +0500

    domain-test

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
   title: "Where committed investors invest",
   description: "Built for investors who mean business",
   other: {
-    "backlinkexchange-verification": "2cd7d8e2-26fc-4711-b32a-93dd45bb8c6b"
+    "backlinkexchange-verification": "21a0f6af-4227-46c7-a534-aa725fc811e0"
   }
 };
 
